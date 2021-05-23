a6c662a3ba964ccbaa4d1b112f31ff8d58182dc7
Jonathan Terner <jon.programming@gmail.com>
2021-05-23 18:58:47 -0400

    Added gitignore, recursion, and private functions

--- hello_world.ex
+++ hello_world.ex
@@ -5,5 +5,21 @@ defmodule HelloWorld do
     puts(msg)
   end
   def short_say_hi(msg), do: say_hi(msg)
+
+  # quick example of private functions and recursion
+  def repeat_hi_3, do: say_hi_n("Hello World", 3)
+  defp say_hi_n(msg, 1), do: say_hi(msg)
+  defp say_hi_n(msg, n) do
+    say_hi(msg)
+    say_hi_n(msg, n-1)
+  end
+
+end
+
+defmodule MyMath do
+  def power(x, n \\ 2)
+  def power(_x, 0), do: 1
+  def power(x , n), do: x * power(x, n-1)
+
 end
 
